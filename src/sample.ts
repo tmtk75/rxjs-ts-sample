b3249aaff873918f2f6f1f066572e8ac864ee9c9
Tomotaka Sakuma <ktmtmks@gmail.com>
2020-04-17 22:07:28 +0900

    interval sample.

--- src/sample.ts
+++ src/sample.ts
@@ -1,5 +1,13 @@
-import { interval, timer, Observable, from } from "rxjs";
-import { scan, zip, bufferCount, map, flatMap } from "rxjs/operators";
+import { interval, timer, Observable, from, of } from "rxjs";
+import {
+  scan,
+  zip,
+  bufferCount,
+  map,
+  flatMap,
+  reduce,
+  tap,
+} from "rxjs/operators";
 
 // const p = Observable.timer(0, DELAY)  // Not equal to interval(delay)
 // .zip(Observable.from(objs).bufferCount(PARALLEL))
@@ -8,12 +16,17 @@ import { scan, zip, bufferCount, map, flatMap } from "rxjs/operators";
 // .do(e => bar.tick());
 // return p.scan((a, b) => a.concat(b)).toPromise();  // Collect all chunks as an array.
 
+const save = (i: number) => of(i * 10).toPromise();
+
 const delay = 1000 * 0.5;
-const objs = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const objs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 (async () => {
   const p = interval(delay).pipe(
     zip(from(objs).pipe(bufferCount(3))),
-    // flatMap((e) => e)
+    map(([, e]) => e),
+    flatMap((chunks) => Promise.all(chunks.map((i) => save(i))))
   );
-  p.pipe().subscribe(console.log);
+  p.pipe(scan((a, b) => a.concat(b)))
+    .pipe(map((e) => [e.length, e]))
+    .subscribe(console.log);
 })();
